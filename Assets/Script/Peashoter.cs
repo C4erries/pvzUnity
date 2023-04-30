9e2eeeb0b05f4e001b34abcc4220e14f461a3b11
C4erries <85022477+C4erries@users.noreply.github.com>
2023-04-30 22:36:24 +0300

    shoot enemy checking

--- Assets/Script/Peashoter.cs
+++ Assets/Script/Peashoter.cs
@@ -6,16 +6,20 @@ public class Peashoter : MonoBehaviour
 {
     public GameObject peaBullet;
     public Canvas canvas;
+    public bool enemySeen = false;
     void Start()
     {
         canvas = this.GetComponentInParent<Canvas>();
         { InvokeRepeating("Shoot", 0, 2.0f); }
+        enemySeen = false;
     }
 
     // Update is called once per frame
     private void Shoot()
     {
-        Instantiate(peaBullet, this.gameObject.transform);
-        
+        if(enemySeen)
+        { 
+            Instantiate(peaBullet, this.gameObject.transform); 
+        } 
     }
 }
