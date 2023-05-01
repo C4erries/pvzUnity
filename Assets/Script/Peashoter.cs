7752142a2668a8b85c43a5f35af9f298cf904f67
C4erries <85022477+C4erries@users.noreply.github.com>
2023-05-01 15:46:16 +0300

    3

--- Assets/Script/Peashoter.cs
+++ Assets/Script/Peashoter.cs
@@ -6,12 +6,16 @@ public class Peashoter : MonoBehaviour
 {
     public GameObject peaBullet;
     public Canvas canvas;
-    public bool enemySeen = false;
+    public bool enemySeen = true;
     void Start()
     {
         canvas = this.GetComponentInParent<Canvas>();
+<<<<<<< HEAD
         { InvokeRepeating("Shoot", 0, 2.0f); }
-        enemySeen = false;
+        enemySeen = true;
+=======
+        { InvokeRepeating("Shoot", 0, 1.0f); }
+>>>>>>> 86e658494f7664159ef6cdbcf7bcfd34dd894fb7
     }
 
     // Update is called once per frame
